d28159e113e13ecfadfbff502887d8352658416a
petedigipulse <peter@digitalpulse.co.nz>
2015-07-12 21:02:40 +1200

    Set list key

--- mailer/app/controllers/application_controller.rb
+++ mailer/app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  @mc = Mailchimp::API.new('e66a49d3c53f53ea3e172c278af314ae-us10')
+  # @mc = Mailchimp::API.new('e66a49d3c53f53ea3e172c278af314ae-us10')
+  MAILCHIMP-LIST-ID = "e66a49d3c53f53ea3e172c278af314ae-us10"
+  MAILCHIMP-LIST-ID = "0322578d69"
+
+  mailchimp = Mailchimp::API.new(MAILCHIMP-API-KEY)
+
 end
